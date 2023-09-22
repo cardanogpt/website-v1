45954de180dcae8137c388b4c8cfe222733c6a99
Hector <adeniyi.olaitanhector@yahoo.com>
2023-09-22 12:33:21 +0100

    hide buy cgi button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
             <li><a href='#ecosystem'>Ecosystem</a></li>
             <li><a target='__blank' href='https://docs.cardanogpt.ai/' >Docs</a></li>
         </ul>
-        <MainButton target='onTab' className='modal-button' href='https://sale.cardanogpt.ai/' text='Buy CGI' />
+        {/* <MainButton target='onTab' className='modal-button' href='https://sale.cardanogpt.ai/' text='Buy CGI' /> */}
         
         <CgMenu onClick={toggleNav} className='menu-button' id='menu' size='4rem' />
         
@@ -45,7 +45,7 @@ const Nav = () => {
             {/* <Popup trigger={<button className='modal-button'>Buy CGI</button>} position="right center" modal>
             <div className='popup'>Coming Soon!!</div>
             </Popup> */}
-            <MainButton href='https://sale.cardanogpt.ai/' text='Buy CGI' />
+            {/* <MainButton href='https://sale.cardanogpt.ai/' text='Buy CGI' /> */}
         </div>
 
     </NavBar>
