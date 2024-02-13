e063d58530a27eed1cd33687dd7e6e962bc93511
Hector <adeniyi.olaitanhector@yahoo.com>
2024-02-13 15:27:01 +0100

    Update Buy.jsx with new swap URL

--- src/page/Buy.jsx
+++ src/page/Buy.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 const Buy = () => {
     useEffect(() => {
-        window.location.replace('https://app.wingriders.com/swap/ada/2d587111358801114f04df83dc0015de0a740b462b75cce5170fc935434749/constantProduct');
+        window.location.replace('https://app.dexhunter.io/swap?tokenIdSell=&tokenIdBuy=2d587111358801114f04df83dc0015de0a740b462b75cce5170fc935434749');
     });
     return (
         null    
